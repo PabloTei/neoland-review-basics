b18bd7226d27dcdfa15ce25977fbdf0acace48e3
PabloTei <pteijeirolopez@gmail.com>
2023-01-16 18:27:39 +0100

    iteration10

--- app.js
+++ app.js
@@ -121,11 +121,11 @@ console.log(contFavoritesSounds)
 
 // Iteración #4: Métodos findArrayIndex   ------- NO ME SALE
 
-function findArrayIndex([array], text) {
+function findArrayIndex(array, text) {
    return array.indexOf(text);
     
 }
-console.log(findArrayIndex(['Caracol', 'Mosquito', 'Salamandra', 'Ajolote'], "Mosquito"))
+console.log(findArrayIndex(['Caracol', 'Mosquito', 'Salamandra', 'Ajolote'], "Ajolote"))
 
 // Iteración #5: Función rollDice
 
